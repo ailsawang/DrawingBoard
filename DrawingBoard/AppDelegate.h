989dfb8c1c96aa06f62398182582961bfd790002
chunhong.wang <chunhong.wang@renren-inc.com>
2016-02-15 09:50:16 +0800

    Initial Commit

---
+++ DrawingBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawingBoard
+//
+//  Created by renren on 16/2/15.
+//  Copyright © 2016年 chunhong.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawingBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawingBoard
+//
+//  Created by renren on 16/2/15.
+//  Copyright © 2016年 chunhong.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
